966aa938746d0df4d93dd9f15c903f4ee07f280a
Danylo <danylohromyak12@gmail.com>
2023-07-04 18:52:54 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Container } from "@mui/material";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <Container bgcolor="primary.main">
+      <Header />
+    </Container>
+  );
+}
+
+export default App;
