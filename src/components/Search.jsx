567db2dbba3da0f08f5f926ecfb47712ca38233a
Danylo <danylohromyak12@gmail.com>
2023-07-04 20:58:22 +0300

    unuseful commit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,22 @@
+import { InputAdornment, IconButton, TextField } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+
 function Search() {
   return (
-    <div>Search</div>
-  )
+    <TextField
+      size="small"
+      sx={{ backgroundColor: 'white', borderRadius: '5px' }}
+      label="Search..."
+      InputProps={{
+        endAdornment: (
+          <InputAdornment>
+            <IconButton >
+              <SearchIcon />
+            </IconButton>
+          </InputAdornment>
+        ),
+      }}
+    />
+  );
 }
-export default Search
+export default Search;
