321db5878965c14f6d0c04c08c7c08dae57b79e6
Danylo <danylohromyak12@gmail.com>
2023-07-05 18:55:10 +0300

    mui commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Container } from "@mui/material";
 import Header from "./components/Header";
 
+// axios.defaults.baseURL = "https://api.brawlstars.com/v1";
+
 function App() {
   return (
     <Container bgcolor="primary.main">
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,22 +1,51 @@
-import { InputAdornment, IconButton, TextField } from '@mui/material';
+import { MenuItem, InputAdornment, IconButton, TextField, Box, FormControl, InputLabel, Select } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+import { useState } from 'react';
 
 function Search() {
+  const [stats, setStats] = useState("Player");
+
+  function handleChange(e) {
+    // if user doesnt click on the menuItem, then the default value is "Player"
+    if (e.target.value === "Player" || e.target.value === "Club") {
+      setStats(e.target.value);
+    }
+  }
+
   return (
-    <TextField
-      size="small"
-      sx={{ backgroundColor: 'white', borderRadius: '5px' }}
-      label="Search..."
-      InputProps={{
-        endAdornment: (
-          <InputAdornment>
-            <IconButton >
-              <SearchIcon />
-            </IconButton>
-          </InputAdornment>
-        ),
-      }}
-    />
+    <Box sx={{
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    }}>
+      <Box>
+      <FormControl sx={{ m: 0.5, minWidth: 100, }}>
+        <InputLabel>{stats}</InputLabel>
+        <Select
+          onChange={handleChange}
+          autoWidth
+          
+        >
+          <MenuItem value={"Player"}>Player</MenuItem>
+          <MenuItem value={"Club"}>Club</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+      <TextField
+        size="small"
+        sx={{ backgroundColor: 'white', borderRadius: '5px' }}
+        placeholder="Search..."
+        InputProps={{
+          endAdornment: (
+            <InputAdornment>
+              <IconButton >
+                <SearchIcon />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+    </Box>
   );
 }
 export default Search;
